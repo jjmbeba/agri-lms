082f2e35bcaffaeef3148871b289797dd7110cca
John Johnson <jjmbeba@gmail.com>
2025-09-10 16:44:32 +0300

    change font to outfit

---
+++ src/components/features/auth/auth-logo.tsx
@@ -0,0 +1,17 @@
+import { GalleryVerticalEnd } from "lucide-react";
+import Link from "next/link";
+
+const AuthLogo = () => {
+  return (
+    <div className="flex justify-center gap-2 md:justify-start">
+      <Link className="flex items-center gap-2 font-medium" href="/">
+        <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+          <GalleryVerticalEnd className="size-4" />
+        </div>
+        Agri LMS
+      </Link>
+    </div>
+  );
+};
+
+export default AuthLogo;
