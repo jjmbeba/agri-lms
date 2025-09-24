a725e6f1876a9f7cc4c309570c9f66367e5bf093
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2025-09-24 19:16:20 +0300

    change request password reset functionality to convex backend

--- convex/convex.config.ts
+++ convex/convex.config.ts
@@ -1,7 +1,9 @@
+import resend from "@convex-dev/resend/convex.config";
 import { defineApp } from "convex/server";
 import betterAuth from "./betterAuth/convex.config";
 
 const app = defineApp();
 app.use(betterAuth);
+app.use(resend);
 
 export default app;
