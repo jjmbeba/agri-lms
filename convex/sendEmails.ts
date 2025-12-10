9c84c09ec8881fc5884788d67a08b21b7e077048
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2025-12-10 20:32:08 +0300

    configure basic convex-resend email

--- convex/sendEmails.ts
+++ convex/sendEmails.ts
@@ -7,7 +7,7 @@ export const resend: Resend = new Resend(components.resend, {});
 export const sendTestEmail = internalMutation({
   handler: async (ctx) => {
     await resend.sendEmail(ctx, {
-      from: "Me <test@mydomain.com>",
+      from: "Notifications <alerts@notifications.aatiupskill.com>",
       to: "delivered@resend.dev",
       subject: "Hi there",
       html: "This is a test email",
